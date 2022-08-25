9e2437cbdee2bca3b62e29758cc39c0ceb81d8ea
Besufekad_Terefe <besalu20@gmail.com>
2022-08-25 10:53:48 -0700

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    addition = 0
-    for i in range(1, len(argv)):
-        addition += int(argv[i])
-        print("{}".format(addition))
+    n = 1
+    num = 0
+    while n != len(argv):
+        a = argv[n]
+        num += int(a)
+        n += 1
+        print("{:d}".format(num))
