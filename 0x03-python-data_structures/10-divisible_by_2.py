f39ea153b6279ef6badfa9c2ec0a408a18cb3541
Besufekad_Terefe <besalu20@gmail.com>
2022-08-30 03:59:33 -0700

    find the biggest and finds all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check = []
-    for i in my_list:
-        if i % 2 == 0:
-            check.append(True)
-        else:
-            check.append(False)
-            return check
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return None
-    tmp = my_list[0]
-    for i in my_list:
-        if i > tmp:
-            tmp = i
-            return tmp
+    return (min(my_list, key=lambda i: -i)) if my_list else None
