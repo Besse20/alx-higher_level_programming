db9ba5ed422aca0317dbe9c32e9af06ec035f005
Besufekad_Terefe <besalu20@gmail.com>
2022-08-30 04:29:19 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    sum = 0
     from sys import argv
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
-        print(sum)
+    print("{}".format(sum(int(a) for a in argv[1:])))
