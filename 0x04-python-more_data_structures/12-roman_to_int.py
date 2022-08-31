fcff36eff1d745a791f20bd5fd49520da17761f2
Besufekad_Terefe <besalu20@gmail.com>
2022-08-31 11:41:40 -0700

    weight average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
-    if (not isinstance(roman_string, str) or
-        roman_string is None):
-        return (0)
-    
-    roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    num = 0
-    
+    number = 0
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return 0
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-        
-        if (i != (len(roman_string) - 1) and
-            roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
-            
-        else:
-            num += roman_dict[roman_string[i]]
-            return (num)
+        if roman_string[i] == "M":
+            number += 1000
+            if roman_string[i-1] == "C" and i != 0:
+                number -= 200
+                if roman_string[i] == "D":
+                    number += 500
+                    if roman_string[i-1] == "C" and i != 0:
+                        number -= 200
+                        if roman_string[i] == "C":
+                            number += 100
+                            if roman_string[i-1] == "X" and i != 0:
+                                number -= 20
+                                if roman_string[i] == "L":
+                                    number += 50
+                                    if roman_string[i-1] == "X" and i != 0:
+                                        number -= 20
+                                        if roman_string[i] == "X":
+                                            number += 10
+                                            if roman_string[i-1] == "I" and i != 0:
+                                                number -= 2
+                                                if roman_string[i] == "V":
+                                                    number += 5
+                                                    if roman_string[i-1] == "I" and i != 0:
+                                                        number -= 2
+                                                        if roman_string[i] == "I":
+                                                            number += 1
+                                                            return number
