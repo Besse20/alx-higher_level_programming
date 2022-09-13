ffd3ea44b583247c1a416d6321fecc46372727df
Besufekad_Terefe <besalu20@gmail.com>
2022-09-13 09:54:03 -0700

    class Square that defines a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""
-Square class definition
-"""
-
-
 class Square:
-    """
-    Square class with private instance attribute size
-    """
-
     def __init__(self, size=0):
-        """
-        Args:
-            size: size of the square
-        """
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
 
-            def area(self):
-                """
-                Returns area of the square instance
-                """
-                return (self.__size ** 2)
+        self.__size = size
+
+        def area(self):
+            ar = self.__size * self.__size
+            return ar
