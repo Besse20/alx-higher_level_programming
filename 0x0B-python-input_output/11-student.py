0fec902386da2b10b516e1d60ce0de55a2970ae0
Besufekad_Terefe <besalu20@gmail.com>
2022-09-28 12:18:31 -0700

    final commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,7 +10,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
+        def to_json(self, attrs=None):
             """retrieves a dictionary representation of
             Student instance"""
+            if type(attrs) == list and all(type(elem) == str for elem in attrs):
+                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
             return self.__dict__
+
+        def reload_from_json(self, json):
+            """replaces all attributes of the student instance"""
+            for key, value in json.items():
+                setattr(self, key, value)
