73b94733ea3abc78b071e235a558e310e1c6b94d
Besufekad_Terefe <besalu20@gmail.com>
2022-09-28 11:38:29 -0700

    class student

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+import json
+from sys import argv
 """
-a script that adds all arguments to a Python list, and then save them to a file
+This file contains a function that adds
+all arguments to a python list and saves
+to a file
 """
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+
 try:
-    obj = load_from_json_file("add_item.json")
-except ValueError:
-    save_to_json_file([], "add_item.json")
-    obj = load_from_json_file("add_item.json")
-    h = []
-    for i in range(1, len(sys.argv)):
-        h.append(sys.argv[i])
-        obj = obj + h
-        save_to_json_file(obj, "add_item.json")
+    new = load_json(filename)
+except (ValueError, FileNotFoundError):
+    new = []
+
+    new = new + argv[1:]
+    save_json(new, filename)
