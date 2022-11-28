14831ce66a6eae8a9b3737078df4f989970f2019
Besufekad_Terefe <besalu20@gmail.com>
2022-11-28 10:52:27 -0800

    last commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = arr.length;
+let i = 0;
+
+for (i = 0; i < length; i++) {
+  console.log(arr[i]);
+}
