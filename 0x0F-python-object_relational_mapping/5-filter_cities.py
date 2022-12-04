a54ea4af9efb1a80247a56068280fe1b71c6580a
Besufekad_Terefe <besalu20@gmail.com>
2022-12-04 11:04:17 -0800

    getting thing done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """
-Script that lists all `cities` in the `cities` table of `hbtn_0e_4_usa`
-where the city's state matches the argument `state name`.
+Script that takes in name of state as argument and list
+all cities of that state
 """
 
-import sys
-import MySQLdb
 
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    state_name = sys.argv[4]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-
-    cur.execute("SELECT c.name \
-    FROM cities c INNER JOIN states s \
-    ON c.state_id = s.id WHERE s.name = %s\
-    ORDER BY c.id", (state_name, ))
-    rows = cur.fetchall()
-
-    for i in range(len(rows)):
-        print(rows[i][0], end=", " if i + 1 < len(rows) else "")
-        print("")
+    check = (argv[4], )
+    cur.execute("SELECT * FROM cities JOIN states\
+    ON cities.state_id = states.id WHERE states.name = %s\
+    ORDER BY cities.id ASC", check)
+    lst = cur.fetchall()
+    cities = []
+    for r in lst:
+        if r[4] == check[0]:
+            cities.append(r[2])
+            print(', '.join(cities))
+            cur.close()
+            db.close()
